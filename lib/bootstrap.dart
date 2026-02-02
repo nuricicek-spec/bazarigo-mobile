f3346325b18aafc34bf682e96992bfa756290462
Adriatica Marine <nuricicek@gmail.com>
2026-02-02 08:57:01 +0400

    Update bootstrap.dart

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -33,19 +33,3 @@ Future<BootstrapResult> bootstrapApp({bool enableSSL = true}) async {
     failedServices: [],
   );
 }
-
-// lib/app.dart (BOŞ - şimdilik)
-import 'package:flutter/material.dart';
-
-class BazarigoApp extends StatelessWidget {
-  const BazarigoApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(child: Text('App')),
-      ),
-    );
-  }
-}
