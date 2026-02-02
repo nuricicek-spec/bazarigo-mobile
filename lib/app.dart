03c8bdfd80ea75bfae90183a0a92358886f002cf
Adriatica Marine <nuricicek@gmail.com>
2026-02-02 08:56:32 +0400

    Create initial BazarigoApp structure in app.dart

---
+++ lib/app.dart
@@ -0,0 +1,15 @@
+// lib/app.dart (BOŞ - şimdilik)
+import 'package:flutter/material.dart';
+
+class BazarigoApp extends StatelessWidget {
+  const BazarigoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(child: Text('App')),
+      ),
+    );
+  }
+}
