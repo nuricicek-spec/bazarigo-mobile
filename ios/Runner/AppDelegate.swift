4857e12b1cd50290422b05acadd2a883f4353bd8
Adriatica Marine <nuricicek@gmail.com>
2026-02-04 16:56:04 +0400

    Update AppDelegate.swift

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,11 +1,14 @@
 import UIKit
 import Flutter
-import FirebaseCore  // Firebase import eklenecek
+import FirebaseCore
 
 @UIApplicationMain
 class AppDelegate: FlutterAppDelegate {
-  override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-    FirebaseApp.configure()  // Firebase başlatma eklenecek
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    FirebaseApp.configure()
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
